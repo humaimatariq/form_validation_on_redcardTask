71e288a6a1d5ad66675132212dfa371e12cde53d
Humaima Tariq <108565220+humaimatariq@users.noreply.github.com>
2024-12-05 12:25:17 +0500

    Update2 script.js

--- script.js
+++ script.js
@@ -28,47 +28,52 @@ function validateform() {
     textarea5.style.backgroundColor = "";
 
     if (a == "") {
-        alert("Name is required");
+        document.getElementById("name1").innerHTML="Name is required";
+        name1.focus();
     } 
     else if (!isNaN(a)) {
-        alert("Enter name only in text");
-        console.log(a);
+        document.getElementById("name1").innerHTML="Enter name only in text";
+        name1.focus();
     } 
     else if (b == "") {
-        alert("Email is required");
+        document.getElementById("email1").innerHTML="Email is required";
+        email1.focus();
     } 
     else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(b)) {
-        alert("Invalid Email Address");
+        document.getElementById("email1").innerHTML="Invalid Email Address";
+        email1.focus();
     } 
     else if (c == "") {
-        alert("Phone is required");
+        document.getElementById("phone1").innerHTML="Phone is required";
+        phone1.focus();
     } 
-    else if (!/^0\d{10}$/.test(c)) {
-        alert("Phone Number should be of 11 digits and start with 0");
-        document.getElementById("phone").focus();
+    else if (!/^0\d{10}$/.test(c)) {  
+        document.getElementById("phone1").innerHTML="Phone Number should be of 11 digits and start with 0";
+        phone1.focus();
     } 
+
     else if (i.length > 150) {
-        alert("Max limit is 150 words");
+        document.getElementById("tax1").innerHTML="Max limit is 150 words";
         textarea1.style.backgroundColor = "blue";
         textarea1.focus();
     } 
     else if (j.length > 150) {
-        alert("Max limit is 150 words");
+        document.getElementById("tax2").innerHTML="Max limit is 150 words";
         textarea2.style.backgroundColor = "blue";
         textarea2.focus();
     } 
     else if (k.length > 150) {
-        alert("Max limit is 150 words");
+        document.getElementById("tax3").innerHTML="Max limit is 150 words";
         textarea3.style.backgroundColor = "blue";
         textarea3.focus();
     } 
     else if (l.length > 150) {
-        alert("Max limit is 150 words");
+        document.getElementById("tax4").innerHTML="Max limit is 150 words";
         textarea4.style.backgroundColor = "blue";
         textarea4.focus();
     } 
     else if (m.length > 150) {
-        alert("Max limit is 150 words");
+        document.getElementById("tax5").innerHTML="Max limit is 150 words";
         textarea5.style.backgroundColor = "blue";
         textarea5.focus();
     }
